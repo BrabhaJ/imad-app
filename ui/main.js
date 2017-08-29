e114a72e1a01e20a254dcfec45878abd32fe7dde
BrabhaJ <30937365+BrabhaJ@users.noreply.github.com>
2017-08-29 16:21:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ var marginLeft = 0;
 
 function moveRight(){
     marginLeft =marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
+    img1.style.marginLeft = marginLeft + 'px';
 }
-var img = document.getElementById("img1");
-img.onClick = function() {
+var img1 = document.getElementById("img1");
+img1.onClick = function() {
     //img.style.marginLeft = "100px";
     
     var Interval = setInterval(moveRight, 100);
