7acacd49c6a83b9717a6f5c77aac06978c98e37a
BrabhaJ <30937365+BrabhaJ@users.noreply.github.com>
2017-08-29 16:00:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById("maintext");
 element.innerHTML = "New txt updated";
+
+var img = document.getElementById("img1");
+img.onClick = function() {
+    img1.style.marginLeft = "100px";
+}
